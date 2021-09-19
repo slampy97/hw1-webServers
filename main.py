ddcec02bc4561e3ef1bc777f60990dc684939464
slampy97 <slampy97@gmail.com>
2021-09-19 21:55:17 +0300

    add status code

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
-from flask import Flask, jsonify
-from flask_restful import Api, Resource
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
+mall_put_args = reqparse.RequestParser()
+mall_put_args.add_argument("id", type=int, help="id of item is required", required=True)
+mall_put_args.add_argument("name", type=str, help="id of item is required", required=True)
+mall_put_args.add_argument("discount", type=float, help="id of item is required", required=True)
+mall = {}
+
+
+def abort_item_id_not_exists(item_id):
+    if item_id not in mall:
+        abort(404, message="item id is not exists!")
+
+
+def abort_item_exists(item_id):
+    if item_id in mall:
+        abort(409, message="already have item with this id")
+
 
 class HelloWorld(Resource):
-    def get(self):
-        return {"data" : "Hello world!"}
+    def get(self, item_id):
+        abort_item_id_not_exists(item_id)
+        return mall[item_id]
 
-    def post(self):
-        return {"data": "posted!"}
+    def put(self, item_id):
+        abort_item_exists(item_id)
+        args = mall_put_args.parse_args()
+        mall[item_id] = args
+        return mall[item_id], 201
+
+    def delete(self, item_id):
+        abort_item_id_not_exists(item_id)
+        del mall[item_id]
+        return '', 204
 
 
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(HelloWorld, "/mall/<int:item_id>")
+
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,16 @@ import requests
 
 Base = "http://127.0.0.1:5000/"
 
-response = requests.post(Base + "helloworld")
+moc_data = [{"name":"potato", "discount":0.7, "id":1},
+            {"name":"tomato", "discount":0.1, "id":2},
+            {"name":"ququmba", "discount":0.2, "id":3}]
+
+for i in range(len(moc_data)):
+    response = requests.put(Base + "mall/" + str(i), moc_data[i])
+    print(response.json())
+input()
+response = requests.delete(Base + "mall/0")
+print(response)
+input()
+response = requests.get(Base + "mall/1")
 print(response.json())
